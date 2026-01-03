39053ac68190cbcaad8bcfa6bd0ae65b7a14a839
Dajmen Graus <dahgraus2002@gmail.com>
2026-01-03 23:51:18 +0100

    Time: 4 ms (53.30%) | Memory: 55.4 MB (59.04%) - LeetSync

---
+++ 1-two-sum/two-sum.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let map = new Map();
+    for (let i = 0; i < nums.length; i++) {
+        let complement = target - nums[i];
+        if (map.has(complement)) {
+            return [i, map.get(complement)]
+        }
+        map.set(nums[i], i);
+    }
+};
